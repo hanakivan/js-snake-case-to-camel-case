2c7a082780a7e1fe29543c77f04a8a912f31a260
Ivan Hanak <hanakivan@gmail.com>
2020-08-05 13:29:46 +0200

    test suite

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,17 @@
 import {snakeCaseToCamelCase} from './index';
 
+const TEST_CASES = {
+    "text-transform": "textTransform",
+    "backgroundColor": "backgroundColor",
+    "text_transform": "text_transform",
+    "text-transform-ellipsys": "textTransformEllipsys",
+};
 
-test('tests case', () => {
-    const myString = "text-transform";
+Object.entries(TEST_CASES).forEach(pair => {
+    const source = pair[0];
+    const result = pair[1];
 
-    const camelCase = snakeCaseToCamelCase(myString);
-    expect(camelCase).toBe("textTransform");
+    test(`test case: ${source}: ${result}`, () => {
+        expect(snakeCaseToCamelCase(source)).toBe(result);
+    });
 });
